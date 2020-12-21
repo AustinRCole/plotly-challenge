63f42e772636910dd58e66675b9c9b482157f806
AustinRCole <AustinRCole2@gmail.com>
2020-12-21 16:59:44 -0500

    actually working now

--- static/js/app.js
+++ static/js/app.js
@@ -7,15 +7,18 @@ d3.json("samples.json").then((data) => {
       var age = metadata.map(row => row.age);
       var bbtype = metadata.map(row => row.bbtype);
       var location = metadata.map(row => row.location);
+      var otu_ids = data.samples.map(row => row.otu_ids);
+      var sample_values = data.samples.map(row => row.sample_values);
+      var otu_labels = data.samples.map(row => row.otu_labels);
 
 
-    for (i = 0; i < data.samples.length; i++) {
-      var otu_ids = data.samples.map(row => row.otu_ids)[i].sort((a,b) => b - a).slice(0,10);
-      var sample_values = data.samples.map(row => row.sample_values).slice(0,10);
-      var otu_labels = data.samples.map(row => row.otu_labels).slice(0,10);
+      var select = d3.select('select');
 
-      console.log(otu_ids[0]);
-      
+      ids.forEach(id => {
+        var entry = select.append('option');
+        entry.text(id)
+      });
+
       function init() {
         var trace1 = [{
           x: otu_ids[0],
@@ -26,7 +29,9 @@ d3.json("samples.json").then((data) => {
         }];
       
         var layout1 = {
-            title: `Top 10 OTU Values for ID number ${ids[0]}`
+            title: `OTU Values for ID number ${ids[0]}`,
+            xaxis: {title: 'Samples'},
+            yaxis: {title: 'Operational Taxonomic Units'}
         };
 
         var trace2 =[{
@@ -39,52 +44,67 @@ d3.json("samples.json").then((data) => {
           },
           text: otu_labels[0]
         }]
+
+        var layout2 = {
+          xaxis: {title: 'Operational Taxonomic Units'},
+          yaxis: {title: 'Sample Values'}
+        };
       
         Plotly.newPlot("bar", trace1, layout1);
 
-        Plotly.newPlot("bubble",trace2)
+        Plotly.newPlot("bubble",trace2, layout2);
 
 
         var container = d3.select('#sample-metadata');
         container.text(`ID: ${ids[0]}; Age: ${age[0]}; Ethnicity: ${ethnicity[0]}; Gender: ${gender[0]}; Belly Button Type: ${bbtype[0]}; Location: ${location[0]}`);
       }
 
-      var select = d3.select('select');
-
-      ids.forEach(id => {
-        var entry = select.append('option');
-        entry.text(id)
-      });
-  
       d3.selectAll("#selDataset").on("change", optionChanged);
-  
-    function optionChanged() {
-      var dropdownMenu = d3.select("#selDataset");
-      
-      var dataset = dropdownMenu.property("value");
-      
-      var x = [];
-      var y = [];
-      var text = [];
-    
-      if (dataset === ids) {
-          x = otu_ids,
-          y = sample_values,
-          text = otu_labels;
-      }
-  
-      updatePlotly(x, y, text);
-    }
-    
-    
-    function updatePlotly(newdata) {
-      Plotly.restyle("bar", "values", [newdata]);
-    }
-  
-      init();
 
-    }
+      function optionChanged() {
+        var id = select.property("value");
+        
+        for (i=0;i<ids.length;i++) {
+        
+          if (id === `${ids[i]}`) {
+          var trace1 = [{
+            x: otu_ids[i],
+            y: sample_values[i],
+            text: otu_labels[i],
+            type: "bar",
+            orientation: 'h'
+          }];
+        
+          var layout1 = {
+              title: `OTU Values for ID number ${ids[i]}`,
+              xaxis: {title: 'Operational Taxonomic Units'},
+              yaxis: {title: 'Samples'}
+          };
   
+          var trace2 =[{
+            x: otu_ids[i],
+            y: sample_values[i],
+            mode: 'markers',
+            marker: {
+              size: sample_values[i],
+              color: otu_ids[i]
+            },
+            text: otu_labels[i]
+          }]
+        
+          Plotly.newPlot("bar", trace1, layout1);
+
+          Plotly.newPlot("bubble",trace2)
+  
+  
+          var container = d3.select('#sample-metadata');
+          container.text(`ID: ${ids[i]}; Age: ${age[i]}; Ethnicity: ${ethnicity[i]}; Gender: ${gender[i]}; Belly Button Type: ${bbtype[i]}; Location: ${location[i]}`);
+        }
+        }
+      }
+
+      init();
+    
   });
 
   
